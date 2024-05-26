8dad5655542081ee6a1350a50cdda9ca57c350b3
Alberto Trabacchin <alberto.trabacchin@gmail.com>
2024-05-26 16:57:24 +0900

    Updated code.

--- bdd100k_new/prepare_dataset.py
+++ bdd100k_new/prepare_dataset.py
@@ -3,15 +3,10 @@ import os
 import shutil
 from tqdm import tqdm
 from PIL import Image
+from pathlib import Path
 import argparse
+import cv2
 
-# Paths to the dataset
-train_annotation_file = '/home/alberto/datasets/bdd100k/labels/det_20/det_train.json'
-val_annotation_file = '/home/alberto/datasets/bdd100k/labels/det_20/det_val.json'
-train_image_directory = '/home/alberto/datasets/bdd100k/images/100k/train/'
-val_image_directory = '/home/alberto/datasets/bdd100k/images/100k/val/'
-output_directory = '/home/alberto/datasets/bdd100k/custom_dataset/'
-test_image_directory = '/home/alberto/datasets/bdd100k/images/100k/test/'
 
 # Desired combination
 desired_combination = {
@@ -84,34 +79,53 @@ def prepare_custom_dataset(annotation_path, img_dir, output_dir, size):
     pbar.close()
 
 
-def prepare_unlabeled_set(img_dir, output_dir, size):
+def prepare_unlabeled_set(vids_dir, output_dir, size):
+    videos = list(vids_dir.glob('*.mov'))
+    sample_fac = 10
+    size = 600
+    pbar = tqdm(total=len(videos))
     # Create output directories
-    os.makedirs(output_dir, exist_ok=True)
+    output_dir.mkdir(parents=True, exist_ok=True)
+    for v in videos:
+        video_name = v.stem
+        # Construct source and destination paths       
+        cap = cv2.VideoCapture(str(v))
+        frames = []
+        counter = 0
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                break
+            if counter % sample_fac == 0:
+                frames.append(frame)
+        cap.release()
 
-    # List all images
-    images = os.listdir(img_dir)
-
-    pbar = tqdm(total=len(images))
-    # Process each image
-    for image_name in images:
-        # Construct source and destination paths
-        src_path = os.path.join(img_dir, image_name)
-        dst_path = os.path.join(output_dir, image_name)
-
-        with Image.open(src_path) as img:
-            # Resize the image to be square
-            img = img.resize((size, size), Image.Resampling.LANCZOS)
-            img.save(dst_path)
+        # Resize the frames to be square
+        for i, frame in enumerate(frames):
+            frame = cv2.resize(frame, (size, size), interpolation=cv2.INTER_LANCZOS4)
+            f_dir = output_dir / f"{video_name}_{i * sample_fac}.png"
+            cv2.imwrite(str(f_dir), frame)
         pbar.update(1)
     pbar.close()
 
 
+
 if __name__ == '__main__':
     args = parse_args()
+
+    # Paths to the dataset
+    train_annotation_file = Path(f'{args.data_path}/labels/det_20/det_train.json')
+    val_annotation_file = Path(f'{args.data_path}/labels/det_20/det_val.json')
+    train_image_directory = Path(f'{args.data_path}/images/100k/train/')
+    val_image_directory = Path(f'{args.data_path}/images/100k/val/')
+    output_directory = Path(f'{args.data_path}/custom_dataset/')
+    test_image_directory = Path(f'{args.data_path}/images/100k/test/')
+    videos_directory = Path(f'{args.data_path}/videos/train/')
+
     # Run the dataset preparation
     # print('Preparing custom train dataset...')
-    # prepare_custom_dataset(train_annotation_file, train_image_directory, f"{output_directory}/train", desired_size)
+    # prepare_custom_dataset(train_annotation_file, train_image_directory, output_directory/"train", desired_size)
     # print('Preparing custom validation dataset...')
-    # prepare_custom_dataset(val_annotation_file, val_image_directory, f"{output_directory}/val", desired_size)
+    # prepare_custom_dataset(val_annotation_file, val_image_directory, output_directory/"val", desired_size)
     print('Preparing custom unlabeled dataset...')
-    prepare_unlabeled_set(test_image_directory, f"{output_directory}/unlabeled", desired_size)
+    prepare_unlabeled_set(videos_directory, output_directory/"unlabeled", desired_size)
